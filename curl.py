045aa61cf893620f6f31cd05afb3824fd8440603
Alexandr Gordienko <red@ip404.ru>
2026-02-09 13:28:14 +0300

    Initial commit

---
+++ curl.py
@@ -0,0 +1,5 @@
+main_site = "https://stellarburgers.education-services.ru/" # главная страница
+register_page = "https://stellarburgers.education-services.ru/register"  # форма регистрации
+login_page = "https://stellarburgers.education-services.ru/login"  # форма авторизации
+password_recovery = "https://stellarburgers.education-services.ru/forgot-password"  # форма восстановления пароля
+account_profile = 'https://stellarburgers.education-services.ru/account/profile' # личный кабинет авторизованного пользователя
