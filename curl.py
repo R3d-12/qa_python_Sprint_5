ed364d00507e16a4c1acba96bc8a5000d7fbba81
Alexandr Gordienko <red@ip404.ru>
2026-02-09 13:29:48 +0300

    Initialize main as empty base

--- curl.py
+++
@@ -1,5 +0,0 @@
-main_site = "https://stellarburgers.education-services.ru/" # главная страница
-register_page = "https://stellarburgers.education-services.ru/register"  # форма регистрации
-login_page = "https://stellarburgers.education-services.ru/login"  # форма авторизации
-password_recovery = "https://stellarburgers.education-services.ru/forgot-password"  # форма восстановления пароля
-account_profile = 'https://stellarburgers.education-services.ru/account/profile' # личный кабинет авторизованного пользователя
