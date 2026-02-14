551b32971b1ca89bb53e497595fe6b213f342fcc
Alexandr Gordienko <red@ip404.ru>
2026-02-14 15:56:12 +0300

    Fix: correct module is specified

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-from helper import generate_registration_data
+from loghelper import generate_registration_data
 from locators import Locators
 from curl import *
 
