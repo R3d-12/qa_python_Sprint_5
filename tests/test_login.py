52c1838fa0a10e4c7261cc01ef3b734f5a61f249
Alexandr Gordienko <red@ip404.ru>
2026-02-14 16:23:40 +0300

    Fix: changed method

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,34 +10,55 @@ from data import Credentials
 @pytest.mark.parametrize('log_button', [Locators.LOGIN_TO_ACCOUNT_BTN, Locators.ACCOUNT_LINK])
 def test_login_on_the_main_page(driver,log_button):
     driver.find_element(*log_button).click()
-    WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(login_page))
+    try:
+        WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(login_page))
+    except Exception:
+        pass
+    assert driver.current_url == login_page, "Не перешли на страницу входа"
     driver.find_element(*Locators.EMAIL_INPUT_LOGIN).send_keys(*Credentials.email_user)
     driver.find_element(*Locators.PASSWORD_INPUT_LOGIN).send_keys(*Credentials.password_user)
     driver.find_element(*Locators.AUTHORIZATION_SUBMIT_BTN).click()
 
-    WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(main_site))
+    try:
+        WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(main_site))
+    except Exception:
+        pass
     assert driver.current_url == main_site, "Ошибка, авторизация не прошла"
 
 # вход по кнопке в форме регистрации
 def test_login_from_registration_form(driver):
     driver.get(register_page)
-    driver.find_element(*Locators.LOGIN_LINK).click()
-    WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(login_page))
+    driver.find_element(*Locators.LOGIN_LINK_IN_FORM).click()
+    try:
+        WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(login_page))
+    except Exception:
+        pass
+    assert driver.current_url == login_page, "Не перешли на страницу входа"
     driver.find_element(*Locators.EMAIL_INPUT_LOGIN).send_keys(*Credentials.email_user)
     driver.find_element(*Locators.PASSWORD_INPUT_LOGIN).send_keys(*Credentials.password_user)
     driver.find_element(*Locators.AUTHORIZATION_SUBMIT_BTN).click()
 
-    WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(main_site))
+    try:
+        WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(main_site))
+    except Exception:
+        pass
     assert driver.current_url == main_site, "Ошибка, авторизация не прошла"
 
 # вход по кнопке в форме восстановления пароля
-def test_login_from_password_recovery_form (driver):
+def test_login_from_password_recovery_form(driver):
     driver.get(password_recovery)
     driver.find_element(*Locators.RECOVERY_LOGIN_LINK).click()
-    WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(login_page))
+    try:
+        WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(login_page))
+    except Exception:
+        pass
+    assert driver.current_url == login_page, "Не перешли на страницу входа"
     driver.find_element(*Locators.EMAIL_INPUT_LOGIN).send_keys(*Credentials.email_user)
     driver.find_element(*Locators.PASSWORD_INPUT_LOGIN).send_keys(*Credentials.password_user)
     driver.find_element(*Locators.AUTHORIZATION_SUBMIT_BTN).click()
 
-    WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(main_site))
+    try:
+        WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(main_site))
+    except Exception:
+        pass
     assert driver.current_url == main_site, "Ошибка, авторизация не прошла"
