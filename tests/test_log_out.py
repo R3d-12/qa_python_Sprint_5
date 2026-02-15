a186f18415738664e15cbcd9d0da683a8096724b
Alexandr Gordienko <red@ip404.ru>
2026-02-15 18:23:12 +0300

    fixed

--- tests/test_log_out.py
+++ tests/test_log_out.py
@@ -9,8 +9,7 @@ def test_personal_account_of_an_authorized_user(driver,login):
     driver.find_element(*Locators.ACCOUNT_LINK).click()
     WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(account_profile))
     driver.find_element(*Locators.PROFILE_LOGOUT_BTN).click()
-    try:
-        WebDriverWait(driver, 20, poll_frequency=0.1).until(EC.url_to_be(login_page))
-    except Exception:
-        pass
+    WebDriverWait(driver, 20, poll_frequency=0.1).until(
+        EC.visibility_of_element_located(Locators.AUTHORIZATION_SUBMIT_BTN)
+    )
     assert driver.current_url == login_page, "Ошибка, выход из аккаунта не получился"
